691df973edb90b685fed7e5b9d06b88c162d93d8
addhes <setiawan.ade0101@gmail.com>
2020-11-17 14:04:25 +0700

    edit delay

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,35 +45,35 @@ function scrollActive(){
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 2000,
+    duration: 1200,
     reset: false
 })
 
 /*SCROLL HOME*/
 sr.reveal('.home__title', {})
-sr.reveal('.home__scroll', {delay: 200})
-sr.reveal('.home__img', {origin:'left', delay: 400})
+sr.reveal('.home__scroll', {delay: 100})
+sr.reveal('.home__img', {origin:'left', delay: 200})
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
-sr.reveal('.about__subtitle', {delay: 300})
-sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
-sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+sr.reveal('.about__img', {delay: 250})
+sr.reveal('.about__subtitle', {delay: 100})
+sr.reveal('.about__profession', {delay: 200})
+sr.reveal('.about__text', {delay: 300})
+sr.reveal('.about__social-icon', {delay: 300, interval: 100})
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 400})
+sr.reveal('.skills__img', {delay: 300})
 
 /*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', {interval: 200})
+sr.reveal('.portfolio__img', {interval: 100})
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval: 200})
-sr.reveal('.contact__input', {delay: 400})
-sr.reveal('.contact__button', {delay: 600})
+sr.reveal('.contact__input', {delay: 300})
+sr.reveal('.contact__button', {delay: 400})
 
 
 
